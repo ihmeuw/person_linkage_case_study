3aa8c64a6f1a0363e1db62901b0d50316b0b21d7
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-03-23 21:22:58 -0700

    add convert_to_variable_name function

--- src/vivarium_research_prl/utils.py
+++ src/vivarium_research_prl/utils.py
@@ -1,5 +1,8 @@
 # Use this to see how much memory the dataframes use
 from sys import getsizeof
+import collections
+import re
+
 
 def sizemb(obj, mib=False):
     """Use sys.getsizeof to return the memory usage of an object
@@ -9,3 +12,15 @@ def sizemb(obj, mib=False):
     """
     mb = 2**20 if mib else 10**6
     return getsizeof(obj) / mb
+
+def convert_to_variable_name(string):
+    """Converts a string to a valid Python variable.
+
+    Strings of non-word characters (regex matchs \W+) are converted to '_',
+    and '_' is appended to the beginning of the string if the string starts
+    with a digit (regex matches ^(?=\d)).
+
+    Solution copied from here:
+    https://stackoverflow.com/questions/3303312/how-do-i-convert-a-string-to-a-valid-variable-name-in-python
+    """
+    return re.sub('\W+|^(?=\d)', '_', string)
