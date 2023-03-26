2b9d26b8a8f7431e43fb10de6b14fa9635995aea
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-03-25 20:24:50 -0700

    add MappingViaAttributes class

--- src/vivarium_research_prl/utils.py
+++ src/vivarium_research_prl/utils.py
@@ -24,3 +24,37 @@ def convert_to_variable_name(string):
     https://stackoverflow.com/questions/3303312/how-do-i-convert-a-string-to-a-valid-variable-name-in-python
     """
     return re.sub('\W+|^(?=\d)', '_', string)
+
+class MappingViaAttributes(collections.abc.Mapping):
+    """Implementation of the Mapping abstract base class that
+    stores mapping keys as object attributes. This is a convenience
+    whose main purpose is to allow tab completion of mapping keys
+    in an interactive coding environment.
+    """
+    def __init__(self, mapping):
+        """Create a MappingViaAttributes object from a dictionary
+        or other implementation of Mapping that maps keys to values.
+        Dictionary keys become object attributes that store the
+        corresponding dictionary values.
+
+        The keys in `mapping` must be valid Python variable names
+        (this is not enforced in the constructor, but bad variable
+        names will cause problems when trying to access the values
+        via attributes).
+        """
+        self.__dict__ = dict(mapping)
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def to_dict(self):
+        return dict(self.__dict__)
+
+    def keylist(self):
+        return list(self.keys())
