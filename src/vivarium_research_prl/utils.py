ee2bee0d1768873a53f4bf9db30f7fd055b7ad60
Zeb Burke-Conte <zmbc@uw.edu>
2024-02-12 11:06:02 -0800

    Refactor pseudopeople generation to use distributed_compute helpers

--- src/vivarium_research_prl/utils.py
+++ src/vivarium_research_prl/utils.py
@@ -96,3 +96,13 @@ def remove_path(path):
         os.remove(path)
     elif path.exists():
         shutil.rmtree(path)
+
+def ensure_empty(path):
+    remove_path(path)
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+def dedupe_list(list_to_dedupe):
+    """Removes duplicates from a list, preserving order."""
+    # https://stackoverflow.com/a/6765391/
+    seen = {}
+    return [seen.setdefault(x, x) for x in list_to_dedupe if x not in seen]
