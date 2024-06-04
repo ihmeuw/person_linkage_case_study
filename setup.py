00af3c06b1487f28f14dcbec15e2744eb9b3ade5
Zeb Burke-Conte <zmbc@uw.edu>
2024-06-03 20:38:34 -0700

    Run black

--- setup.py
+++ setup.py
@@ -17,54 +17,54 @@ if __name__ == "__main__":
 
     install_requirements = [
         # Core libraries
-        'pandas',
-        'pyarrow',
-        'numpy',
-        'matplotlib',
-        'pseudopeople',
-        'splink',
-        'jellyfish',
+        "pandas",
+        "pyarrow",
+        "numpy",
+        "matplotlib",
+        "pseudopeople",
+        "splink",
+        "jellyfish",
         # Workflow management
-        'snakemake',
-        'papermill',
+        "snakemake",
+        "papermill",
         # Pins
-        'pulp<2.8',  # Needed for snakemake, see https://github.com/snakemake/snakemake/issues/2607#issuecomment-1948732242
+        "pulp<2.8",  # Needed for snakemake, see https://github.com/snakemake/snakemake/issues/2607#issuecomment-1948732242
     ]
     dask_requirements = [
-        'pseudopeople[dask]',
-        'dask_jobqueue',
-        'bokeh!=3.0.*,>=2.4.2',  # needed for dask dashboard
+        "pseudopeople[dask]",
+        "dask_jobqueue",
+        "bokeh!=3.0.*,>=2.4.2",  # needed for dask dashboard
     ]
     spark_requirements = [
-        'pyspark==3.4.1',  # NOTE: I have no idea why, but pyspark 3.5.0 (with the correct version of Spark) would hang forever on the first stage
+        "pyspark==3.4.1",  # NOTE: I have no idea why, but pyspark 3.5.0 (with the correct version of Spark) would hang forever on the first stage
     ]
     dev_requirements = (
         [
-            'jupyterlab',
-            'nbdime',
-            'black[jupyter]',
+            "jupyterlab",
+            "nbdime",
+            "black[jupyter]",
         ]
         + dask_requirements
         + spark_requirements
     )
 
     setup(
-        name=about['__title__'],
-        version=about['__version__'],
-        description=about['__summary__'],
+        name=about["__title__"],
+        version=about["__version__"],
+        description=about["__summary__"],
         long_description=long_description,
-        license=about['__license__'],
+        license=about["__license__"],
         url=about["__uri__"],
         author=about["__author__"],
         author_email=about["__email__"],
-        package_dir={'': 'src'},
-        packages=find_packages(where='src'),
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
         include_package_data=True,
         install_requires=install_requirements,
         extras_require={
-            'dask': dask_requirements,
-            'spark': spark_requirements,
-            'dev': dev_requirements,
+            "dask": dask_requirements,
+            "spark": spark_requirements,
+            "dev": dev_requirements,
         },
         zip_safe=False,
     )
